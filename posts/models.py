569e6b9d00709031a269ea99153aa4e5550c35b9
ildarick93 <saliakhovif@yandex.ru>
2021-02-19 10:58:10 +0300

    first code review

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=100, null=False)
+    title = models.CharField(max_length=200, null=False)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
